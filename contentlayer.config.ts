99167724ba49823b148bf0e39b70738ad7eab64b
Daniel Harms <jdharms@gmail.com>
2023-08-04 17:32:57 -0400

    move about page to a general 'random top level page' route

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,8 +1,13 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 
+const removeMdx = function(name: string) {
+    return name.replace(".mdx", "");
+}
+
+
 export const Post = defineDocumentType(() => ({
   name: 'Post',
-  filePathPattern: `**/posts/*.md`,
+  filePathPattern: `posts/*.mdx`,
   fields: {
     title: { type: 'string', required: true },
     summary: { type: 'string', required: false },
@@ -10,16 +15,23 @@ export const Post = defineDocumentType(() => ({
     tags: { type: 'string', required: false }
   },
   computedFields: {
-    slug: { type: 'string', resolve: (post) => post._raw.sourceFileName.substring(0, post._id.indexOf('.'))},
-    url: { type: 'string', resolve: (post) => `/posts/${post._raw.sourceFileName.substring(0, post._id.indexOf('.'))}` },
+    slug: { type: 'string', resolve: (post) => `${removeMdx(post._raw.sourceFileName)}`},
+    url: { type: 'string', resolve: (post) => `/posts/${removeMdx(post._raw.sourceFileName)}` },
     tagsList: { type: 'list', resolve: (post) => post.tags?.split(" ")}
   },
 }))
 
-export const About = defineDocumentType(() => ({
-    name: 'About',
-    filePathPattern: `about.md`,
-    contentType: 'mdx'
+export const Page = defineDocumentType(() => ({
+    name: 'Page',
+    filePathPattern: `pages/*.mdx`,
+    contentType: 'mdx',
+    fields: {
+        title: { type: 'string', required: true },
+        description: { type: 'string', required: false},
+    },
+    computedFields: {
+        slug: { type: 'string', resolve: (page) => `${removeMdx(page._raw.sourceFileName)}`},
+    }
 }))
 
-export default makeSource({ contentDirPath: 'content', documentTypes: [Post, About] })
+export default makeSource({ contentDirPath: 'content/', documentTypes: [Post, Page] })
